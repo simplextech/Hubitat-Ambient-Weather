219a9adb84fe3f910bd2da59cf21c9925400bf5a
John <jeubanks@simplextech.net>
2019-01-21 14:49:27 -0500

    removed unnecessary ';' as not needed with Groovy

--- AmbientWeatherApp.groovy
+++ AmbientWeatherApp.groovy
@@ -1,38 +1,38 @@
-definition(name: "Ambient Weather API", namespace: "CordMaster", author: "Alden Howard", description: "A simple api for providing ambient weather access", iconUrl: "", iconX2Url: "");
+definition(name: "Ambient Weather API", namespace: "CordMaster", author: "Alden Howard", description: "A simple api for providing ambient weather access", iconUrl: "", iconX2Url: "")
 
 preferences {
     page(name: "page1", title: "Log In", nextPage: "page2", uninstall: true) {
         section {
-            input(name: "applicationKey", title: "Application Key", type: "text", required: true);
-            input(name: "apiKey", title: "API Key", type: "text", required: true);
+            input(name: "applicationKey", title: "Application Key", type: "text", required: true)
+            input(name: "apiKey", title: "API Key", type: "text", required: true)
         }
     }
     
-    page(name: "page2");
-    page(name: "page3");
+    page(name: "page2")
+    page(name: "page3")
 }
 
 def page2() {
-    def stations = [];
-    def stationMacs = [];
+    def stations = []
+    def stationMacs = []
     try {
-        stations = getStations();
+        stations = getStations()
         
-        stations.each { stationMacs << it.macAddress };
+        stations.each { stationMacs << it.macAddress }
     } catch(groovyx.net.http.HttpResponseException e) {
         //then unauthorized
         return dynamicPage(name: "page2", title: "Error", nextPage: "page1", uninstall: true) {
             section {
-                paragraph("There was an error authorizing you. Please try again.");
+                paragraph("There was an error authorizing you. Please try again.")
             }
         }
     }
     
-   	log.debug("Got stations: " + stations);
+   	log.debug("Got stations: " + stations)
     
 	return dynamicPage(name: "page2", title: "Select Station", nextPage: "page3", uninstall: true) {
 		section {
-			input(name: "station", title: "Station", type: "enum", options: stationMacs, required: true);
+			input(name: "station", title: "Station", type: "enum", options: stationMacs, required: true)
 		}
 	}
 }
@@ -40,65 +40,65 @@ def page2() {
 def page3() {
     dynamicPage(name: "page3", title: "Confirm Settings", install: true, uninstall: true) {
         section {
-            paragraph("Selected station: $station");
+            paragraph("Selected station: $station")
         }
         
         section {
-            paragraph("Press done to finish");
+            paragraph("Press done to finish")
         }
     }
 }
 
 //lifecycle functions
 def installed() {
-    log.debug("Installed");
+    log.debug("Installed")
     
-    addDevice();    
-    initialize();    
-    runEvery5Minutes(fetchNewWeather);
+    addDevice()    
+    initialize()    
+    runEvery5Minutes(fetchNewWeather)
 }
 
 def updated() {
-    log.debug("Updated");
+    log.debug("Updated")
     
-    unsubscribe();
-    initialize(); 
+    unsubscribe()
+    initialize() 
 }
 
 def initialize() {
-    fetchNewWeather();
+    fetchNewWeather()
 }
 
 //children
 def addDevice() {
-    addChildDevice("CordMaster", "Ambient Weather Tile", "AWTILE-$station", null, [completedSetup: true]);
+    addChildDevice("CordMaster", "Ambient Weather Tile", "AWTILE-$station", null, [completedSetup: true])
 }
 
 //fetch functions
 def getStations() throws groovyx.net.http.HttpResponseException {
-    def data = [];
+    def data = []
     
     def params = [
         uri: "https://api.ambientweather.net/",
         path: "/v1/devices",
         query: [applicationKey: applicationKey, apiKey: apiKey]
-    ];
+    ]
     
     requestData("/v1/devices", [applicationKey: applicationKey, apiKey: apiKey]) { response ->
-        data = response.data;
-    };
+        data = response.data
+    }
         
-    return data;
+    return data
 }
 
 def getWeather() throws groovyx.net.http.HttpResponseException {
-    def data = [];
+    def data = []
     
     requestData("/v1/devices/$station", [applicationKey: applicationKey, apiKey: apiKey, limit: 1]) { response ->
-        data = response.data;
-    };
+        data = response.data
+    }
         
-	return data[0];
+	return data[0]
 }
 
 def requestData(path, query, code) {
@@ -106,48 +106,48 @@ def requestData(path, query, code) {
         uri: "https://api.ambientweather.net/",
         path: path,
         query: query
-    ];
+    ]
     
     httpGet(params) { response ->
-        code(response);
-    };
+        code(response)
+    }
 }
 
 //loop
 def fetchNewWeather() {
-    log.debug("Getting new weather...");
+    log.debug("Getting new weather...")
     
-    def weather = getWeather();
+    def weather = getWeather()
     
-    log.debug("Weather: " + weather);
+    log.debug("Weather: " + weather)
     
-    // childDevices[0].setTemperature(weather.tempf);
-    // childDevices[0].setHumidity(weather.humidity);
+    // childDevices[0].setTemperature(weather.tempf)
+    // childDevices[0].setHumidity(weather.humidity)
 
     //
     // Simplex Technology
     //
-    childDevices[0].setBaromabsin(weather.baromabsin);
-    childDevices[0].setBaromrelin(weather.baromrelin);
-    childDevices[0].setDailyrainin(weather.dailyrainin);
+    childDevices[0].setBaromabsin(weather.baromabsin)
+    childDevices[0].setBaromrelin(weather.baromrelin)
+    childDevices[0].setDailyrainin(weather.dailyrainin)
     // Need to normalize date
-    //childDevices[0].setDate(weather.date);    
-    childDevices[0].setDewPoint(weather.dewPoint);
-    childDevices[0].setFeelsLike(weather.feelsLike);
-    childDevices[0].setHourlyrainin(weather.hourlyrainin);
-    childDevices[0].setHumidity(weather.humidity);
-    childDevices[0].setHumidityin(weather.humidityin);
+    //childDevices[0].setDate(weather.date)    
+    childDevices[0].setDewPoint(weather.dewPoint)
+    childDevices[0].setFeelsLike(weather.feelsLike)
+    childDevices[0].setHourlyrainin(weather.hourlyrainin)
+    childDevices[0].setHumidity(weather.humidity)
+    childDevices[0].setHumidityin(weather.humidityin)
     // Need to normalize the date
-    //childDevices[0].setLastRain(weather.lastRain);    
-    childDevices[0].setMaxdailygust(weather.maxdailygust);
-    childDevices[0].setMonthlyrainin(weather.monthlyrainin);
-    childDevices[0].setSolarradiation(weather.solarradiation);
-    childDevices[0].setTemperature(weather.tempf);
-    childDevices[0].setTempinf(weather.tempinf);
-    childDevices[0].setTotalrainin(weather.totalrainin);
-    childDevices[0].setUV(weather.uv);
-    childDevices[0].setWeeklyrainin(weather.weeklyrainin);
-    childDevices[0].setWinddir(weather.winddir);
-    childDevices[0].setWindgustmph(weather.windgustmph);
-    childDevices[0].setWindspeedmph(weather.windspeedmph);
+    //childDevices[0].setLastRain(weather.lastRain)    
+    childDevices[0].setMaxdailygust(weather.maxdailygust)
+    childDevices[0].setMonthlyrainin(weather.monthlyrainin)
+    childDevices[0].setSolarradiation(weather.solarradiation)
+    childDevices[0].setTemperature(weather.tempf)
+    childDevices[0].setTempinf(weather.tempinf)
+    childDevices[0].setTotalrainin(weather.totalrainin)
+    childDevices[0].setUV(weather.uv)
+    childDevices[0].setWeeklyrainin(weather.weeklyrainin)
+    childDevices[0].setWinddir(weather.winddir)
+    childDevices[0].setWindgustmph(weather.windgustmph)
+    childDevices[0].setWindspeedmph(weather.windspeedmph)
 }
--- AmbientWeatherDevice.groovy
+++ AmbientWeatherDevice.groovy
@@ -42,104 +42,104 @@ metadata {
     tiles {
 		
         standardTile("refresh", "device.switch") {
-            state("default", label: "Refresh", action: "refresh");
+            state("default", label: "Refresh", action: "refresh")
         }
         
         // valueTile("temperature", "device.temperature", width: 2, height: 2) {
-        //     state("temperature", label: '${currentValue}', unit: "dF");
+        //     state("temperature", label: '${currentValue}', unit: "dF")
         // }
     }
 }
 
 def refresh() {
-	parent.fetchNewWeather();
+	parent.fetchNewWeather()
 }
 
 //
 //Simplex Technology
 //
 def setBaromabsin(value) {
-    sendEvent(name: "Abs Barometric Pressure", value: value);
+    sendEvent(name: "Abs Barometric Pressure", value: value)
 }
 
 def setBaromrelin(value) {
-    sendEvent(name: "Rel Barometric Pressure", value: value);
+    sendEvent(name: "Rel Barometric Pressure", value: value)
 }
 
 def setDailyrainin(value) {
-    sendEvent(name: "Daily Rain", value: value);
+    sendEvent(name: "Daily Rain", value: value)
 }
 
 // Need to normalize the date
 // def setDate(value) {
-//     sendEvent(name: "Date", value: value);
+//     sendEvent(name: "Date", value: value)
 // }
 
 def setDewPoint(value) {
-    sendEvent(name: "Dew Point", value: value);
+    sendEvent(name: "Dew Point", value: value)
 }
 
 def setFeelsLike(value) {
-    sendEvent(name: "Feels Like", value: value);
+    sendEvent(name: "Feels Like", value: value)
 }
 
 def setHourlyrainin(value) {
-    sendEvent(name: "Hourly Rain", value: value);
+    sendEvent(name: "Hourly Rain", value: value)
 }
 
 def setHumidityin(value) {
-    sendEvent(name: "HumidityIn", value: value);
+    sendEvent(name: "HumidityIn", value: value)
 }
 
 def setHumidity(value) {
-    sendEvent(name: "Humidity", value: value);
+    sendEvent(name: "Humidity", value: value)
 }
 
 // Need to normalize the date
 // def setLastRain(value) {
-//     sendEvent(name: "Last Rain", value: value);
+//     sendEvent(name: "Last Rain", value: value)
 // }
 
 def setMaxdailygust(value) {
-    sendEvent(name: "Max Daily Gust", value: value);
+    sendEvent(name: "Max Daily Gust", value: value)
 }
 
 def setMonthlyrainin(value) {
-    sendEvent(name: "Monthly Rain", value: value);
+    sendEvent(name: "Monthly Rain", value: value)
 }
 
 def setSolarradiation(value) {
-    sendEvent(name: "Solar Radiation", value: value);
+    sendEvent(name: "Solar Radiation", value: value)
 }
 
 def setTemperature(value) {
-    sendEvent(name: "Temperature", value: value, unit: '°F');
+    sendEvent(name: "Temperature", value: value, unit: '°F')
 }
 
 def setTempinf(value) {
-    sendEvent(name: "TemperatureIn", value: value, unit: '°F');
+    sendEvent(name: "TemperatureIn", value: value, unit: '°F')
 }
 
 def setTotalrainin(value) {
-    sendEvent(name: "Total Rain", value: value);
+    sendEvent(name: "Total Rain", value: value)
 }
 
 def setUV(value) {
-    sendEvent(name: "UV Index", value: value);
+    sendEvent(name: "UV Index", value: value)
 }
 
 def setWeeklyrainin(value) {
-    sendEvent(name: "Weekly Rain", value: value);
+    sendEvent(name: "Weekly Rain", value: value)
 }
 
 def setWinddir(value) {
-    sendEvent(name: "Wind Direction", value: value);
+    sendEvent(name: "Wind Direction", value: value)
 }
 
 def setWindgustmph(value) {
-    sendEvent(name: "Wind Gust MPH", value: value);
+    sendEvent(name: "Wind Gust MPH", value: value)
 }
 
 def setWindspeedmph(value) {
-    sendEvent(name: "Wind Speed MPH", value: value);
+    sendEvent(name: "Wind Speed MPH", value: value)
 }
